83b8e149a643529823a44aefebfa1aa860ba44b4
hungtp <hungtp.personal@gmail.com>
2020-02-29 10:19:22 +0700

    UI

--- lib/pages/channel/channel_page.dart
+++ lib/pages/channel/channel_page.dart
@@ -15,11 +15,12 @@ class ChannelPage extends StatelessWidget {
           List<Channel> channels = state.channels;
           return Container(
             color: primaryColor,
-            child: Center(
-              child: Text(
-                "loaded {${channels.length.toString()}}",
-                style: defaultTextStyle,
-              ),
+            child: ListView.builder(
+              itemCount: channels.length,
+              itemBuilder: (context, index) {
+                Channel channel = channels[index];
+                return _buildChannel(context, channel);
+              },
             ),
           );
         } else {
@@ -34,6 +35,61 @@ class ChannelPage extends StatelessWidget {
           );
         }
       },
+    );
+  }
+
+  Widget _buildChannel(BuildContext context, Channel channel) {
+    return Container(
+      height: 220.0,
+      child: Padding(
+        padding: defaultPadding,
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+                child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              child: Container(
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(channel.thumbnailUrl),
+                ),
+              ),
+            )),
+            Positioned.fill(
+                child: Container(
+              color: primaryColor.withOpacity(0.3),
+            )),
+            Positioned(
+              bottom: 20.0,
+              left: (MediaQuery.of(context).size.width - 230) / 2.0,
+              child: Container(
+                width: 200.0,
+                decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: secondaryColor,
+                          blurRadius: 10.0,
+                          spreadRadius: 1.0,
+                          offset: Offset(0, 0))
+                    ]),
+                child: Center(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 12.0),
+                    child: Text(
+                      channel.title,
+                      maxLines: 3,
+                      style: defaultTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
